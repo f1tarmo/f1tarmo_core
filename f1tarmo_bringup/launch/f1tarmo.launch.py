ccc6797ed5fe7528ea4e8a627f5f306dc352de73
Nathan Litzinger <nlitz88@gmail.com>
2025-05-22 00:31:39 -0400

    Added vesc params to bringup params

--- f1tarmo_bringup/launch/f1tarmo.launch.py
+++ f1tarmo_bringup/launch/f1tarmo.launch.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
@@ -15,6 +16,27 @@ def generate_launch_description():
             default_value=str(Path(get_package_share_directory('f1tarmo_bringup')) / 'params' / 'f1tarmo_params.yaml'),
             description='Path to config file with parameters for the vesc nodes brought up in this launch file'
         ),
+        DeclareLaunchArgument(
+            'log_level',
+            default_value='info',
+            description='Log level for all nodes in this launch file'
+        ),
+        # Include f1tarmo_description launch file to provide static transforms.
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                str(Path(get_package_share_directory('f1tarmo_description')) / 'launch' / 'description.launch.py')
+            )
+        ),
+        # Bring up vehicle controller / command interface node.
+        Node(
+            package='f1tarmo_controllers',
+            executable='bicycle_controller_node',
+            name='bicycle_controller_node',
+            parameters=[LaunchConfiguration('params_file')]
+        ),
+        # Include f1tarmo_controllers launch file to provide bicycle controller
+        # node.
+        # TODO: Update to use composition!!!
         Node(
             package='vesc_ackermann',
             executable='ackermann_to_vesc_node',
