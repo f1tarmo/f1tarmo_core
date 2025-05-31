adb5c3a6cd83e46ad846fea6ff73ce2d560c134e
Nathan Litzinger <nlitz88@gmail.com>
2025-05-30 21:00:51 -0400

    Added f1tarmo bringup within the joystick teleop launch file

--- f1tarmo_bringup/launch/joy_teleop.launch.py
+++ f1tarmo_bringup/launch/joy_teleop.launch.py
@@ -1,7 +1,12 @@
+# Launch file for teleoperating the f1tarmo using a gamepad / console
+# controller. 
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
@@ -24,5 +29,13 @@ def generate_launch_description():
             executable='teleop_node',
             name='joystick_to_twist_node',
             parameters=[LaunchConfiguration('params_file')]
-        )
+        ),
+        # Include f1tarmo_bringup to bring up the rest of the f1tarmo's core
+        # nodes.
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                str(Path(get_package_share_directory('f1tarmo_bringup')) / 'launch' / 'f1tarmo.launch.py')
+            )
+        ),
+
     ])
